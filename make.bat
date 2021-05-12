199a389b7dade52429e2586f821e42c6a7fd5d99
kai <supon3060@gmail.com>
2021-05-12 23:49:30 +0800

    Update help

--- make.bat
+++ make.bat
@@ -8,6 +8,8 @@ if "%1" == "help" (
     echo.Please use `make ^<target^>` where ^<target^> is one of
     echo. conda-update: Create or update conda env
     echo. conda-remove: Remove conda env
+    echo. build-image: Build im2latex/api-server docker image
+    echo. run-container: Run im2latex-api container
     echo.
 )
 
@@ -26,14 +28,14 @@ if "%1" == "conda-remove" (
     goto end
 )
 
-@REM Build api-server docker image
+@REM Build im2latex/api-server docker image
 if "%1" == "build-image" (
     docker rmi -f im2latex/api-server
     docker build -t im2latex/api-server -f api_server/Dockerfile .
     goto end
 )
 
-@REM Build api-server docker image
+@REM Run im2latex-api container
 if "%1" == "run-container" (
     docker rm -f im2latex-api
     docker run -p 8080:8000 -it --rm --name im2latex-api im2latex/api-server
