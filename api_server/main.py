4b6fe68cc56086463f534aab2c00af0866867ba8
kai <supon3060@gmail.com>
2021-05-12 23:53:17 +0800

    Clean up

--- api_server/main.py
+++ api_server/main.py
@@ -27,22 +27,17 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"msg": "Hello World"}
 
 
 @app.get("/v1/predict", response_model=LatexResponse, status_code=status.HTTP_200_OK)
 def get_predict(image_url: str = None):
     if image_url is None or not image_url.startswith("http"):
         raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="no image_url defined in query string")
-    # logging.info("url {}".format(image_url))
     logging.info("url %s", image_url)
     image = util.read_image_pil(image_url, grayscale=True)
     pred = model.predict(image)
     image_stat = ImageStat.Stat(image)
-    # logging.info("METRIC image_mean_intensity {}".format(image_stat.mean[0]))
-    # logging.info("METRIC image_area {}".format(image.size[0] * image.size[1]))
-    # logging.info("METRIC pred_length {}".format(len(pred)))
-    # logging.info("pred {}".format(pred))
     logging.info("METRIC image_mean_intensity %f", image_stat.mean[0])
     logging.info("METRIC image_area %d", image.size[0] * image.size[1])
     logging.info("METRIC pred_length %d", len(pred))
@@ -59,10 +54,6 @@ async def post_predict(json_request: ImageRequest):
     image = util.read_b64_image(json_request.image, grayscale=True)
     pred = model.predict(image)
     image_stat = ImageStat.Stat(image)
-    # logging.info("METRIC image_mean_intensity {}".format(image_stat.mean[0]))
-    # logging.info("METRIC image_area {}".format(image.size[0] * image.size[1]))
-    # logging.info("METRIC pred_length {}".format(len(pred)))
-    # logging.info("pred {}".format(pred))
     logging.info("METRIC image_mean_intensity %f", image_stat.mean[0])
     logging.info("METRIC image_area %d", image.size[0] * image.size[1])
     logging.info("METRIC pred_length %d", len(pred))
