e1a59f7d89da97497f1c18196225cc1cfbd7d42e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 12:33:02 +0000

    Adding tarfile member sanitization to extractall()

--- im2latex/data/im2latex_100k.py
+++ im2latex/data/im2latex_100k.py
@@ -194,7 +194,29 @@ def _process_raw_dataset(metadata: MutableMapping, vocab_filename: Union[Path, s
     if not (PROCESSED_DATA_DIRNAME / "formula_images_processed").is_dir():
         print("Unzipping formula_images_processed.tar.gz...")
         with tarfile.open(img_tarfile, "r:gz") as tar_file:
-            tar_file.extractall(PROCESSED_DATA_DIRNAME)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_file, PROCESSED_DATA_DIRNAME)
 
     # move .lst files to PROCESSED_DATA_DIRNAME
     for _metadata in metadata.values():
